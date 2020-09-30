238189a8cd09b3468a166fe579bcddd218c63c2f
Jim Skretas <jimskretas@gmail.com>
2020-09-30 19:07:00 +0300

    Add and change necessary files for Heroku(and PostgreSQL) (#10)
    - Add configuration for postgresql for different environments(production or other).
    - Fix Procfile that had double quotes for heroku.
    - Add CORS(may need to change that later for security purposes).

--- config/database.php
+++ config/database.php
@@ -1,37 +1,32 @@
 <?php
 
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\App;
+
+$env = getenv('APP_ENV');
+
+if ($env == 'production')
+{
+    $url = env('DATABASE_URL');
+    $parts = parse_url($url);
+
+    $host = $parts["host"];
+    $username = $parts["user"];
+    $password = $parts["pass"];
+    $database = substr($parts["path"], 1);
+
+}
+else
+{
+    $host = env('DB_HOST', '127.0.0.1');
+    $username = env('DB_USERNAME', 'forge');
+    $password = env('DB_PASSWORD', '');
+    $database = env('DB_DATABASE', 'forge');
+}
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Database Connection Name
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify which of the database connections below you wish
-    | to use as your default connection for all database work. Of course
-    | you may use many connections at once using the Database library.
-    |
-    */
-
-    'default' => env('DB_CONNECTION', 'mysql'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Database Connections
-    |--------------------------------------------------------------------------
-    |
-    | Here are each of the database connections setup for your application.
-    | Of course, examples of configuring each database platform that is
-    | supported by Laravel is shown below to make development simple.
-    |
-    |
-    | All database work in Laravel is done through the PHP PDO facilities
-    | so make sure you have the driver for your particular database of
-    | choice installed on your machine before you begin development.
-    |
-    */
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     'connections' => [
 
@@ -66,11 +61,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $host,
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
