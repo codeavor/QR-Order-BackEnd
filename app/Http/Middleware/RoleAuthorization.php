7f9f320a1a2dce9bf97263aafd77c152d6aa348e
george stinis <george.stinis@yahoo.gr>
2020-10-11 23:19:17 +0300

    Fix try-catch in RoleAuthorization to pass codeclimate

--- app/Http/Middleware/RoleAuthorization.php
+++ app/Http/Middleware/RoleAuthorization.php
@@ -25,15 +25,9 @@ class RoleAuthorization
             $token = JWTAuth::parseToken();
             //Try authenticating user       
             $user = $token->authenticate();
-        } catch (TokenExpiredException $e) {
-            //Thrown if token has expired        
-            return $this->unauthorized('Your token has expired. Please, login again.');
-        } catch (TokenInvalidException $e) {
-            //Thrown if token invalid
-            return $this->unauthorized('Your token is invalid. Please, login again.');
-        }catch (JWTException $e) {
-            //Thrown if token was not found in the request.
-            return $this->unauthorized('Please, attach a Bearer Token to your request');
+        } catch (TokenExpiredException | TokenInvalidException | JWTException $e) {
+            //Thrown if token has expired | token is invalid | there is no token at all       
+            return $this->unauthorized($e->getMessage());
         }
         //If user was authenticated successfully and user is in one of the acceptable roles, send to next request.
         if ($user && in_array($user->role->name, $roles)) {
