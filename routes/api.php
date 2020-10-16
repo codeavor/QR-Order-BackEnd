d2186745651831d0e6a66b3bfdcb6e6b776f4fce
george stinis <george.stinis@yahoo.gr>
2020-10-16 20:14:51 +0300

    Re-develop endpoints to require tokens
    - Remove JWT Secret Key from phpunit.xml
    - Configure routes for authorization (needs more work)

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('menu', 'Menu\MenuController')->only(['index', 'show']);
-Route::apiResource('cart', 'ShoppingCart\CartController')->only(['show', 'update', 'destroy']);
-Route::apiResource('order_item', 'OrderItem\OrderItemController')->only(['store','update', 'destroy']);
+Route::middleware('auth.role:customer')->apiResource('menu', 'Menu\MenuController')->only(['index', 'show']);
+Route::middleware('auth.role:customer')->apiResource('cart', 'ShoppingCart\CartController')->only(['show', 'update', 'destroy']);
+Route::middleware('auth.role:customer')->apiResource('order_item', 'OrderItem\OrderItemController')->only(['store','update', 'destroy']);
 Route::post('auth/login', ['uses' => 'AuthController@login', 'as' => 'api_login']);
 Route::post('auth/register', ['uses' => 'AuthController@register', 'as' => 'api_register']);
-Route::post('auth/refresh', ['uses' => 'AuthController@getToken', 'as' => 'api_refresh']);
-//Route::get('umbrella_{id}', 'Umbrella\UmbrellaController@umbrellaById')->name('specific_umbrella');
+Route::middleware('auth.role:service')->post('auth/refresh', ['uses' => 'AuthController@getToken', 'as' => 'api_refresh']);
 
