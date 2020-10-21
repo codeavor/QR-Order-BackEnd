56e055e8a4bb0b8a1d36ea9c6f5d775dfb62eed1
pant377 <pantpap15@gmail.com>
2020-10-21 20:51:02 +0300

    Test finetuning

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth.role:customer')->apiResource('menu', 'Menu\MenuController')->only(['index', 'show']);
-Route::middleware('auth.role:customer')->apiResource('cart', 'ShoppingCart\CartController')->only(['show', 'update', 'destroy']);
-Route::middleware('auth.role:customer')->apiResource('order_item', 'OrderItem\OrderItemController')->only(['store','update', 'destroy']);
+Route::middleware('auth.role:customer,service')->apiResource('menu', 'Menu\MenuController')->only(['index', 'show']);
+Route::middleware('auth.role:customer,service')->apiResource('cart', 'ShoppingCart\CartController')->only(['show', 'update', 'destroy']);
+Route::middleware('auth.role:customer,service')->apiResource('order_item', 'OrderItem\OrderItemController')->only(['store','update', 'destroy']);
 Route::post('auth/login', ['uses' => 'AuthController@login', 'as' => 'api_login']);
 Route::post('auth/register', ['uses' => 'AuthController@register', 'as' => 'api_register']);
 Route::post('auth/logout', ['uses' => 'AuthController@logout', 'as' => 'api_logout']);
